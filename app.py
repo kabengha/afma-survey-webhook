dd9f3cde16a13652d5a41f3f8d1c6ff8f0a221a5
kabengha <50748650+kabengha@users.noreply.github.com>
2026-01-26 12:10:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+import base64
+import json
 from flask import Flask, request, jsonify
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization, hashes
 
 app = Flask(__name__)
 
+# Charger la PRIVATE KEY
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None
+    )
+
 @app.route("/api/survey/webhook", methods=["GET", "POST"])
 def survey_webhook():
+
     if request.method == "GET":
         return jsonify({"status": "ok"}), 200
 
-    # POST (tes messages / flow payload)
-    data = request.get_json(silent=True) or {}
-    return jsonify({"received": True}), 200
+    # 👉 Réponse Flow OBLIGATOIRE
+    response_payload = {
+        "status": "ok"
+    }
+
+    encrypted = private_key.encrypt(
+        json.dumps(response_payload).encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    encrypted_base64 = base64.b64encode(encrypted).decode()
+
+    return jsonify({
+        "encrypted_response": encrypted_base64
+    }), 200
